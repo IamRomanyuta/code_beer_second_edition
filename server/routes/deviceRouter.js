f05c18a72193050d5bff63ed1ad3c98d65a1b857
Andrey Romanyuta <romanyutcka@gmail.com>
2023-04-09 13:27:17 +0300

    added middlewares and auth function

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
 const devicController = require("../controllers/deviceController");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post("/", devicController.create);
+router.post("/", checkRole("ADMIN"), devicController.create);
 router.get("/", devicController.getAll);
 router.get("/:id", devicController.getOne);
 router.delete("/", devicController.deleteOne);
