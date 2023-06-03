13089f71faadb7745a60d950037747822b3ca51c
Andrey Romanyuta <romanyutcka@gmail.com>
2023-06-03 10:02:51 +0300

    easy loader

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import TypeBar from "../components/TypeBar";
 import DeviceList from "../components/DeviceList";
 import "../styles/Shop.css";
@@ -7,14 +7,23 @@ import "../styles/DeviceItem.css";
 import { observer } from "mobx-react-lite";
 import { Context } from "../index";
 import { fetchDevices, fetchTypes } from "../http/deviceAPI";
+import { Spinner } from "react-bootstrap";
 
 const Shop = observer(() => {
   const { device } = useContext(Context);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchTypes().then((data) => device.setTypes(data));
-    fetchDevices().then((data) => device.setDevices(data.rows));
+    fetchDevices()
+      .then((data) => device.setDevices(data.rows))
+      .finally(setLoading(false));
   }, []);
+
+  if (loading) {
+    return <Spinner animation={"grow"} />;
+  }
+
   return (
     <div className="shop-main-window">
       <div className="left-side-menu">
